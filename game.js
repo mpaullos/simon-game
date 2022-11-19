621808c0bf0f517de045a93dba71e559585b927a
Marcos Paulo <82289818+mpaullos@users.noreply.github.com>
2022-11-19 20:12:02 -0300

    add animate on button selection

--- game.js
+++ game.js
@@ -1,13 +1,15 @@
 $("h1");
 var gamePattern = [];
 function nextSequence() {
-    var randomNumber = Math.floor(Math.random() * 3 + 1);
+    var randomNumber = Math.floor(Math.random() * 4);
     return randomNumber;
 }
 
-buttonCollors = ["red", "green", "blue", "yellow"];
-randomChosenColour = buttonCollors[nextSequence()];
+var buttonCollors = ["red", "green", "blue", "yellow"];
+var randomChosenColour = buttonCollors[nextSequence()];
 
 gamePattern.push(randomChosenColour);
+console.log(randomChosenColour)
 
 
+$("#" + randomChosenColour).fadeOut(100).fadeIn(100);
