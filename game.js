75d3502d39f74dacfabed91f29b93d3030284236
Marcos Paulo <82289818+mpaullos@users.noreply.github.com>
2022-11-19 19:44:40 -0300

    create functions

--- game.js
+++ game.js
@@ -1,1 +1,13 @@
-alert("Hello, eu estou funcionando");
+$("h1");
+var gamePattern = [];
+function nextSequence() {
+    var randomNumber = Math.floor(Math.random() * 3 + 1);
+    return randomNumber;
+}
+
+buttonCollors = ["red", "green", "blue", "yellow"];
+randomChosenColour = buttonCollors[nextSequence()];
+
+gamePattern.push(randomChosenColour);
+
+
