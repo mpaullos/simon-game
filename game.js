fd32b2aefe2840e8f6d001e43140ea06df4cd30f
Marcos Paulo <82289818+mpaullos@users.noreply.github.com>
2022-11-24 23:32:23 -0300

    feat: created  function checkAnswer
    ]

--- game.js
+++ game.js
@@ -9,23 +9,26 @@ var level = 0;
 $(".btn").click(function () {
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
-  console.log(userClickedPattern);
   playSound(userChosenColour);
   animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length - 1);
 });
 
 $("body").keypress(function (event) {
   if (event.key.toLowerCase() == "a") {
     $("#level-title").text("Level " + level);
     nextSequence();
-    console.log(gamePattern);
   }
 });
 
 function nextSequence() {
+  level++;
+  $("#level-title").text("Level " + level);
+  userClickedPattern = [];
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonCollors[randomNumber];
   gamePattern.push(randomChosenColour);
+  console.log(gamePattern);
   $("#" + randomChosenColour)
     .fadeOut(100)
     .fadeIn(100)
@@ -45,4 +48,17 @@ function animatePress(currentColor) {
   setTimeout(function () {
     $("#" + currentColor).removeClass("pressed");
   }, 100);
+}
+
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log("Sucess");
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    $("#level-title").text("Parabéns, o seu recorde é " + level + "!");
+  }
 }
